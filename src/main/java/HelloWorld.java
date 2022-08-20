a4ba9da56e7d02c91c72173816b8d83a39602fa0
Harika <harikamutyam@gmail.com>
2022-08-19 23:54:09 -0700

    addressingfeedback

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,10 +2,12 @@
  * Created by raviteja on 6/1/19.
  */
 public class HelloWorld {
+
+    public static final int MAX_COUNT=15;
     public static void main(String[] args) {
 
 
-        for(int i=0; i<15; i++){
+        for(int i=0; i<MAX_COUNT; i++){
             System.out.println(i+ " -> hello harika");
 
         }
