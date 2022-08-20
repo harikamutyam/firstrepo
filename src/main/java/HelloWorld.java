7df8f7f7613ff03a0e76dd50a35391cea1606538
Harika Eswari <51279118+harikamutyam@users.noreply.github.com>
2022-08-20 00:11:41 -0700

    updating loop (#2)
    * updating loop

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,11 +2,13 @@
  * Created by raviteja on 6/1/19.
  */
 public class HelloWorld {
+
+    public static final int MAX_COUNT=15;
     public static void main(String[] args) {
 
 
-        for(int i=0; i<10; i++){
-            System.out.println("hello harika");
+        for(int i=0; i<MAX_COUNT; i++){
+            System.out.println(i+ " -> hello harika");
 
         }
     }
