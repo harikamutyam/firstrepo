dd8998c75643ce0fe834de8bc30055592c4748f7
Harika <harikamutyam@gmail.com>
2022-08-19 23:32:42 -0700

    updating loop

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,8 +5,8 @@ public class HelloWorld {
     public static void main(String[] args) {
 
 
-        for(int i=0; i<10; i++){
-            System.out.println("hello harika");
+        for(int i=0; i<15; i++){
+            System.out.println(i+ " -> hello harika");
 
         }
     }
